b3d294f566d195a7b014299593f24bef0e5af026
Novar29rizad <104239245+Novar29rizad@users.noreply.github.com>
2022-06-15 20:06:01 +0800

    Add files via upload

---
+++ assets/js/app-script.js
@@ -0,0 +1,219 @@
+
+$(function() {
+    "use strict";
+     
+	 
+//sidebar menu js
+$.sidebarMenu($('.sidebar-menu'));
+
+// === toggle-menu js
+$(".toggle-menu").on("click", function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });	 
+	   
+// === sidebar menu activation js
+
+$(function() {
+        for (var i = window.location, o = $(".sidebar-menu a").filter(function() {
+            return this.href == i;
+        }).addClass("active").parent().addClass("active"); ;) {
+            if (!o.is("li")) break;
+            o = o.parent().addClass("in").parent().addClass("active");
+        }
+    }), 	   
+	   
+
+/* Top Header */
+
+$(document).ready(function(){ 
+    $(window).on("scroll", function(){ 
+        if ($(this).scrollTop() > 60) { 
+            $('.topbar-nav .navbar').addClass('bg-dark'); 
+        } else { 
+            $('.topbar-nav .navbar').removeClass('bg-dark'); 
+        } 
+    });
+
+ });
+
+
+/* Back To Top */
+
+$(document).ready(function(){ 
+    $(window).on("scroll", function(){ 
+        if ($(this).scrollTop() > 300) { 
+            $('.back-to-top').fadeIn(); 
+        } else { 
+            $('.back-to-top').fadeOut(); 
+        } 
+    }); 
+
+    $('.back-to-top').on("click", function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});	   
+	    
+   
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+	 // theme setting
+	 $(".switcher-icon").on("click", function(e) {
+        e.preventDefault();
+        $(".right-sidebar").toggleClass("right-toggled");
+    });
+	
+	$('#theme1').click(theme1);
+    $('#theme2').click(theme2);
+    $('#theme3').click(theme3);
+    $('#theme4').click(theme4);
+    $('#theme5').click(theme5);
+    $('#theme6').click(theme6);
+    $('#theme7').click(theme7);
+    $('#theme8').click(theme8);
+    $('#theme9').click(theme9);
+    $('#theme10').click(theme10);
+    $('#theme11').click(theme11);
+    $('#theme12').click(theme12);
+    $('#theme13').click(theme13);
+    $('#theme14').click(theme14);
+    $('#theme15').click(theme15);
+
+    function theme1() {
+      $('body').attr('class', 'bg-theme bg-theme1');
+    }
+
+    function theme2() {
+      $('body').attr('class', 'bg-theme bg-theme2');
+    }
+
+    function theme3() {
+      $('body').attr('class', 'bg-theme bg-theme3');
+    }
+
+    function theme4() {
+      $('body').attr('class', 'bg-theme bg-theme4');
+    }
+	
+	function theme5() {
+      $('body').attr('class', 'bg-theme bg-theme5');
+    }
+	
+	function theme6() {
+      $('body').attr('class', 'bg-theme bg-theme6');
+    }
+
+    function theme7() {
+      $('body').attr('class', 'bg-theme bg-theme7');
+    }
+
+    function theme8() {
+      $('body').attr('class', 'bg-theme bg-theme8');
+    }
+
+    function theme9() {
+      $('body').attr('class', 'bg-theme bg-theme9');
+    }
+
+    function theme10() {
+      $('body').attr('class', 'bg-theme bg-theme10');
+    }
+
+    function theme11() {
+      $('body').attr('class', 'bg-theme bg-theme11');
+    }
+
+    function theme12() {
+      $('body').attr('class', 'bg-theme bg-theme12');
+    }
+	
+	function theme13() {
+      $('body').attr('class', 'bg-theme bg-theme13');
+    }
+	
+	function theme14() {
+      $('body').attr('class', 'bg-theme bg-theme14');
+    }
+	
+	function theme15() {
+      $('body').attr('class', 'bg-theme bg-theme15');
+    }
+
+
+
+
+});
+
+const addBtn = document.getElementById('add')
+
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if(notes){
+    notes.forEach(note => addNewNote(note))
+}
+
+addBtn.addEventListener('click', () => addNewNote('Title'))
+
+function addNewNote(text =''){
+    const note = document.createElement('div')
+    note.classList.add('note')
+
+    note.innerHTML=`
+    <div class="tools">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+
+    <div class="main ${text ? "" : "hidden"}"></div>
+    <textarea class="${text ? "hidden" : ""}"></textarea> 
+    `
+    
+    const editBtn = note.querySelector('.edit')
+    const deleteBtn = note.querySelector('.delete')
+    const main = note.querySelector('.main')
+    const textArea = note.querySelector('textarea')
+
+    textArea.value = text
+    main.innerHTML = marked(text)
+
+    deleteBtn.addEventListener('click', () =>{
+        note.remove()
+
+        updateLS()
+    })
+    
+    editBtn.addEventListener('click', () =>{
+        main.classList.toggle('hidden')
+        textArea.classList.toggle('hidden')
+    })
+
+    textArea.addEventListener('input', (e) => {
+        const { value } = e.target
+
+        main.innerHTML = marked(value)
+
+        updateLS()
+    })
+    
+     document.body.appendChild(note)
+}
+
+function updateLS(){
+    const notesText = document.querySelectorAll('textarea')
+
+    const notes = []
+
+    notesText.forEach(note => notes.push(note.value))
+
+    localStorage.setItem('notes', JSON.stringify(notes))
+
+}
